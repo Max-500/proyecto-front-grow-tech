a12470244ae227fed2444b8e192e1af3cec11e98
Max-500 <213497@ids.upchiapas.edu.mx>
2023-07-08 20:34:02 -0600

    Añadiendo responsividad a login y registro completa (provisional)

--- src/components/ComponentHome.jsx
+++ src/components/ComponentHome.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const ComponentHome = () => {
 
-    const home = <p>Lorem ipsum DE HOME dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
+    const home = <p className="p-home">Lorem ipsum DE HOME dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
         orci eu porta, nam habitasse quis nullam lacinia parturient sed. Mus tincidunt mattis molestie euismod egestas,
         nascetur dictumst vitae magna rhoncus nullam, turpis morbi eleifend nulla. Quisque aptent quam enim donec
         parturient nascetur primis varius, laoreet class dictumst penatibus metus massa potenti faucibus, eros iaculis
@@ -14,7 +14,7 @@ const ComponentHome = () => {
         proin penatibus nullam, sem condimentum bibendum mattis semper ornare fermentum neque interdum, cum auctor
         habitant suspendisse nisl suscipit nam.</p>
 
-    const aboutUs = <p>Lorem ipsum DE ABOUT US dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
+    const aboutUs = <p className="p-home">Lorem ipsum DE ABOUT US dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
         orci eu porta, nam habitasse quis nullam lacinia parturient sed. Mus tincidunt mattis molestie euismod egestas,
         nascetur dictumst vitae magna rhoncus nullam, turpis morbi eleifend nulla. Quisque aptent quam enim donec
         parturient nascetur primis varius, laoreet class dictumst penatibus metus massa potenti faucibus, eros iaculis
@@ -26,7 +26,7 @@ const ComponentHome = () => {
         proin penatibus nullam, sem condimentum bibendum mattis semper ornare fermentum neque interdum, cum auctor
         habitant suspendisse nisl suscipit nam.</p>
 
-    const contactUs = <p>Lorem ipsum DE CONTACT US dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
+    const contactUs = <p className="p-home">Lorem ipsum DE CONTACT US dolor sit amet consectetur adipiscing elit commodo, fringilla pharetra ante pretium convallis
         orci eu porta, nam habitasse quis nullam lacinia parturient sed. Mus tincidunt mattis molestie euismod egestas,
         nascetur dictumst vitae magna rhoncus nullam, turpis morbi eleifend nulla. Quisque aptent quam enim donec
         parturient nascetur primis varius, laoreet class dictumst penatibus metus massa potenti faucibus, eros iaculis
@@ -62,9 +62,7 @@ const ComponentHome = () => {
                 <li className="li-home"><a onClick={handleLinkClickAboutUs} className="a-home" href="/sobre-nosotros">Sobre Nosotros</a></li>
                 <li className="li-home"><a onClick={handleLinkClickContactUs} className="a-home" href="/contactenos">Contáctenos</a></li>
             </ul>
-            <p className="p-home">
                 {linkText}
-            </p>
         </>
     )
 }
